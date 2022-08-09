6d8a292badcda15f21f3611afac250ec99461b1a
sabhafalki <103079095+sabhafalki@users.noreply.github.com>
2022-08-09 13:47:37 -0400

    Adding file to the multiple point

---
+++ Mapping_Multiple_Points/static/js/logic3.js
@@ -0,0 +1,30 @@
+// Drill down : change background to black (Edit the logic.js file to create an orange circle popup marker for each city, with a lineweight of 4, a radius where the population number is decreased by 200,000,  that's on a dark map. When you click on the circle, the popup should display the city, state, and the population formatted with a thousands separator.)
+
+// Add console.log to check to see if our code is working.
+console.log("working");
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function (city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population / 200000,
+        color: 'orange',
+        fillColor: '#FFC000'
+
+    })
+        .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+        .addTo(map);
+});
